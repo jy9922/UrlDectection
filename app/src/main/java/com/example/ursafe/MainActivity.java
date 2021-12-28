71ad3e6f7e768bae6528c7718d9239cd26219a74
myjjy <myjjy6190@gmail.com>
2021-12-28 19:28:58 +0900

    QR코드 스캔

--- app/src/main/java/com/example/ursafe/MainActivity.java
+++ app/src/main/java/com/example/ursafe/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.ursafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button scanQRBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanQRBtn = (Button) findViewById(R.id.goBtn);
+
+        scanQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,ScanQR.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ursafe/ScanQR.java
@@ -0,0 +1,42 @@
+package com.example.ursafe;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ScanQR extends AppCompatActivity {
+
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_qr);
+
+        //new IntentIntegrator(this).initiateScan();
+        qrScan = new IntentIntegrator(this);
+        qrScan.setOrientationLocked(false);
+        qrScan.setPrompt("QR코드를 사각형 안에 넣어주세요.");
+        qrScan.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                // todo
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                // todo
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
+
